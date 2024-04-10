ac276a56231a884b1cf2e0746e64757679358125
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-04-10 16:55:30 +0200

    Add AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "AppTitle",
+  data() {
+    return {
+      myTitle: "La mia prima app con Vite",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ myTitle }}</h1>
+</template>
+
+<style></style>
