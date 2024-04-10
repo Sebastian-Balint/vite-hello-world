cfbc72764d6f2cc060a2f468423b80c48b4d491d
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-04-10 17:04:41 +0200

    Add fix paths

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
-import AppSubtitle from "./components/AppSubtitle.vue";
 
-// Qui va il js
 export default {
   components: {
     AppTitle,
-    AppSubtitle,
   },
 };
 </script>
 
 <template>
-  <!-- qui va l'html -->
   <main>
     <AppTitle></AppTitle>
-    <AppSubtitle></AppSubtitle>
   </main>
 </template>
 
-<style>
-/* qui va il css */
-</style>
+<style></style>
